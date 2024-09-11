143c00c6f17b630d7e402e41bb10206b476db197
Suraj Rampure <surajr@me.com>
2024-09-10 17:40:35 -0700

    Delete homeworks directory

--- homeworks/hw04/tests/q5e.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q5e',
-    'points': 2,
-    'suites': [{'cases': [{'code': '>>> isinstance(poly_prediction_for_350, float)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
