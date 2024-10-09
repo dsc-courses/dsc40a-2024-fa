b354b69de38d863e8fa12349be0fb704b6b03913
zoeludena <zoeludena@gmail.com>
2024-10-09 10:50:33 -0700

    Trying to fix homework 1

--- homeworks/hw01/tests/q8a.py
+++ homeworks/hw01/tests/q8a.py
@@ -1,4 +1,4 @@
-test = {   'name': 'q6a',
+test = {   'name': 'q8a',
     'points': 2,
     'suites': [   {   'cases': [   {'code': '>>> callable(missing_value_naive) and isinstance(missing_value_naive([2, 1, 5, 3]), int)\nTrue', 'hidden': False, 'locked': False},
                                    {   'code': '>>> ex1 = missing_value_naive([2, 1, 5, 3])\n>>> ex2 = missing_value_naive([10, 9, 1, 6, 5, 7, 2, 4, 3])\n>>> ex1 == 4 and ex2 == 8\nTrue',
--- homeworks/hw04/tests/q5a.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q5a',
-    'points': 2,
-    'suites': [   {   'cases': [{'code': '>>> isinstance(prediction_for_350, float) and isinstance(is_accurate, bool)\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
