3c6ffaff2e53a059794dda9125387c398a66aae1
zoeludena <zoeludena@gmail.com>
2024-11-28 00:03:53 -0800

    Changing Hw7

---
+++ homeworks/hw07/tests/q6b.py
@@ -0,0 +1,9 @@
+test = { 'name':'q6b',
+    'points':3,
+    'suites': [  { 'cases': [    {'code': '>>> predict_align(comics, "Public Identity", "Female Characters", "Living Characters", "DC") in ["Good Characters", "Bad Characters"]\nTrue', 'hidden':False, 'locked':False},
+                                {'code': '>>> isinstance(predict_align(comics, "Public Identity", "Female Characters", "Living Characters", "DC"), str)\nTrue', 'hidden':False, 'locked':False},
+                                {'code': '>>> callable(predict_align)\nTrue', 'hidden':False, 'locked':False}],
+                    'scored': True,
+                    'setup':'',
+                    'teardown':'',
+                    'type':'doctest'}]}
